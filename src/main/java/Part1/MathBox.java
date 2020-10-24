0a535bb711f2140fcfcfb77ac90588d35da05403
Anastasiya <korsakova-nastena@yandex.ru>
2020-10-24 11:45:38 +0300

    Конструктор

---
+++ src/main/java/Part1/MathBox.java
@@ -0,0 +1,28 @@
+/**
+ * @author Korsakova Anastasiya
+ * Класс MathBox, реализующий следующий функционал:
+ * Входной массив любого размера
+ * методы Summator, Splitter, Eraser
+ */
+
+package Part1;
+import java.util.*;
+
+
+public class MathBox {
+
+    // Конструктор на вход получающий массив Number
+    public static void main(String[] args){
+
+    }
+
+    // метод summator, возвращающий сумму всех элементов коллекции
+    public double summator(){
+
+        return summator();
+    }
+
+    public int splitter(){
+        
+    }
+}
---
+++ src/main/java/Part1/ObjectBox.java
@@ -0,0 +1,28 @@
+/**
+ * @author Korsakova Anastasiya
+ * Класс ObjectBox, реализующий следующий функционал:
+ * addObject, deleteObject, dump
+ */
+
+package Part1;
+
+public class ObjectBox<T> {
+
+
+    // метод addObject добавляет объект в коллекцию
+    public void addObject(){
+
+
+    }
+
+
+    // метод deleteObject проверяет наличие объекта в коллекции и при наличии удаляющий его
+    public void deleteObject(){
+
+    }
+
+    // метод dump выводит содержимое коллекции в строку
+    public void dump(){
+        System.out.println();
+    }
+}

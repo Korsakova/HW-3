12f49327cd275301b24935d133bcfa005a7aaf13
Anastasiya <korsakova-nastena@yandex.ru>
2020-10-26 14:41:42 +0300

    еще один ПР

--- src/main/java/MathBox.java
+++ src/main/java/MathBox.java
@@ -9,19 +9,49 @@ import java.util.*;
 
 
 public class MathBox {
+    List<Double> numbers;
+
 
     // Конструктор на вход получающий массив Number
-    public static void main(String[] args){
-
+    MathBox(Number[] numbers){
+        Set<Double> otherNumbers = new HashSet<>();
+        for (int k = 0; k < numbers.length; k++) {
+            otherNumbers.add(numbers[k].doubleValue());
+        }
+        this.numbers = new ArrayList<>(otherNumbers);
     }
 
     // метод summator, возвращающий сумму всех элементов коллекции
     public double summator(){
+        double j = 0;
+        for (Double number : numbers) {
+           j = j + number;
+        }
+        return j;
+    }
+    // метод splitter, выполняющий поочередное деление всех хранящихся в объекте элементов на делитель, являющийся аргументом метода.
+    // Хранящиеся в объекте данные полностью заменяются результатами деления.
+    public void splitter(int divider){
+        double a =0;
+        for (int i = 0; i < numbers.size(); i++) {
+            a = numbers.get(i) / divider;
+            numbers.set(i, a);
+        }
 
-        return summator();
     }
 
-    public int splitter(){
-        
+    // метод deleteIntNumber, который получает на вход Integer и если такое значение есть в коллекции, удаляет его.
+    public void deleteIntNumber(Integer numberToDelete){
+        numbers.remove(numberToDelete.doubleValue());
     }
+
+    public static void main(String[] args) {
+        MathBox mathBox = new MathBox(new Number[]{1,2,3,4,5});
+        System.out.println(mathBox.summator());
+        mathBox.splitter(5);
+        System.out.println(mathBox.summator());
+
+    }
+
 }
+
--- src/main/java/ObjectBox.java
+++ src/main/java/ObjectBox.java
@@ -1,26 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * @author Korsakova Anastasiya
  * Класс ObjectBox, реализующий следующий функционал:
  * addObject, deleteObject, dump
  */
 
-public class ObjectBox<T> {
+public class ObjectBox{
+    List<Object> objects;
+
+    // конструктор для объекта objects
+    ObjectBox(){
+        this.objects = new ArrayList<>();
+    }
 
 
     // метод addObject добавляет объект в коллекцию
-    public void addObject(){
-
-
+    public void addObject(Object addInCollection){
+        this.objects.add(addInCollection);
     }
 
-
     // метод deleteObject проверяет наличие объекта в коллекции и при наличии удаляющий его
-    public void deleteObject(){
-
+    public void deleteObject(Object deleteFromCollection){
+        this.objects.remove(deleteFromCollection);
     }
 
     // метод dump выводит содержимое коллекции в строку
-    public void dump(){
-        System.out.println();
+    public String dump(){
+       return this.objects.toString();
+    }
+
+    public static void main(String[] args) {
+        ObjectBox newObjectBox = new ObjectBox();
+        newObjectBox.addObject(5);
+        newObjectBox.addObject("Настя");
+        newObjectBox.addObject(56.0);
+        newObjectBox.deleteObject(1);
+        newObjectBox.deleteObject(56.0);
+        System.out.println(newObjectBox.dump());
     }
 }
